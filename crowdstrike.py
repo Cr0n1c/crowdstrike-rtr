45157703d66c6d6508b9ef67cbf40726b574a4a6
Brandon Helms <bhelms85@gmail.com>
2020-11-18 10:30:42 -0600

    Update crowdstrike.py

--- crowdstrike.py
+++ crowdstrike.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 class Crowdstrike:
 
     def __init__(self, client_id:str, client_secret:str) -> None:
@@ -9,8 +10,10 @@ class Crowdstrike:
         self.hosts_metadata = []
         self.base_url = "https://api.crowdstrike.com"
         self.session = self.__authenticate(client_id, client_secret)
-        
-    def __authenticate(self, client_id:str, client_secret:str) -> None:
+        self.session_timeout = 30
+        self.errors = []
+
+    def __authenticate(self, client_id:str, client_secret:str) -> requests.Session:
         session = requests.Session()
         session.headers["Content-Type"] = "application/x-www-form-urlencoded"
         session.headers["Accept"] = "application/json"
@@ -20,9 +23,13 @@ class Crowdstrike:
         }
 
         response = session.post("{}/oauth2/token".format(self.base_url), data=data)
+
         if response.status_code != 201:
-            print("failed to authenticate")
-            exit()
+            print("[!] Failed to authenticate")
+            self.errors = [response.json()]
+            return None
+        else:
+            self.errors = []
         
         session.headers["Authorization"] = "Bearer {}".format(response.json().get("access_token"))
         session.headers["Content-Type"] = "application/json"
@@ -35,40 +42,74 @@ class Crowdstrike:
         }
 
         response = self.session.get("{}/devices/queries/devices/v1".format(self.base_url), params=params)
+
         if response.status_code != 200:
-            print("failed to get hosts from get_host_ids")
+            print("[!] Failed to get hosts from get_host_ids")
+            self.errors = [response.json()]
         else:
+            self.errors = []
             self.hosts = [host for host in response.json().get('resources')]
 
     def get_host_information(self) -> None:
         params = { "ids": self.hosts}
 
         response = self.session.get("{}/devices/entities/devices/v1".format(self.base_url), params=params)
+
         if response.status_code != 200:
             print("failed to get hosts from get_host_information")
+            self.errors = [response.json()]
         else:
+            self.errors = []
             self.hosts_metadata = response.json().get("resources") 
 
-    def set_session(self, hosts_list:list) -> None:
+    def set_session(self, hosts_list:list, queue_if_offline:bool=True) -> None:
         data = json.dumps({ "host_ids": hosts_list,   
-                            "queue_offline": True})
+                            "queue_offline": queue_if_offline
+                          })
+        
+        response = self.session.post("{}/real-time-response/combined/batch-init-session/v1?timeout={}&timeout_duration={}s".format(
+                                        self.base_url, 
+                                        self.session_timeout, 
+                                        self.session_timeout), 
+                                     data=data)
 
-        response = self.session.post("{}/real-time-response/combined/batch-init-session/v1?timeout=30&timeout_duration=30s".format(self.base_url), data=data)
         if response.status_code == 404:
             print(response.json().get("errors")[0].get("message"))
+            self.errors = [response.json()]
         elif response.status_code != 201:
             print("failed to set session")
+            self.errors = [response.json()]
         else:
+            self.errors = []
             self.batch_id = response.json().get("batch_id")
 
-    def run_command(self, base_command: str, command_options:str) -> dict:
-        data = json.dumps({ "batch_id": self.batch_id, "base_command": base_command, "command_string": command_options, "persist_all": True})
-        print(data)
+    def run_command(self, base_command:str, command_options:str, persist:bool=True) -> dict:
+        self.errors = []
+        data = json.dumps({ "batch_id": self.batch_id, 
+                            "base_command": base_command, 
+                            "command_string": command_options, 
+                            "persist_all": persist
+                          })
 
-        response = self.session.post("{}/real-time-response/combined/batch-admin-command/v1?timeout=30&timeout_duration=30s".format(self.base_url), data=data)
+        response = self.session.post("{}/real-time-response/combined/batch-admin-command/v1?timeout={}&timeout_duration={}s".format(
+                                        self.base_url, 
+                                        self.session_timeout, 
+                                        self.session_timeout), 
+                                     data=data)
+
         if response.status_code != 201:
-            print("failed to execute command")
-            return None
+            self.errors = [response.json()]
+            return {}
         else:
-            return response.json().get('combined').get("resources")
+            resources = response.json().get('combined').get("resources")
+            for k, v in resources.items():
+                if v.get("errors") != [] or v.get("stderr") != "":
+                    for host in self.hosts_metadata:
+                        if host.get("device_id") == k:
+                            self.errors.append({"id": k, 
+                                                "error": v.get("errors"), 
+                                                "stderr": v.get("stderr"), 
+                                                "hostname": host.get("hostname")
+                                              })
 
+            return resources
